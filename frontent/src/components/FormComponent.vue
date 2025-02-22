98a98c3d4af24d33643630076c9750d7eb732ff9
Artem Titariev <artem.titariev@gmail.com>
2025-02-22 13:20:56 +0200

    [frontend] Finilize form

--- frontent/src/components/FormComponent.vue
+++ frontent/src/components/FormComponent.vue
@@ -1,5 +1,12 @@
 <script setup>
+import { useAccountsDealsStore } from "@/stores/accountsDeals";
+import { storeToRefs } from "pinia";
 import { reactive } from "vue";
+
+const store = useAccountsDealsStore();
+
+const { errors, message, isLoading, isSuccess } = storeToRefs(store);
+const { createDealAndAccount } = store;
 
 const formData = reactive({
   account_name: "",
@@ -14,7 +21,18 @@ const formData = reactive({
   <main>
     <h1 class="title">Create account and deal</h1>
 
-    <form @submit.prevent="console.log(formData)" class="w-1/2 mx-auto">
+    <form
+      @submit.prevent="createDealAndAccount(formData)"
+      class="w-1/2 mx-auto"
+    >
+      <p
+        v-if="message"
+        class="message-block"
+        :class="isSuccess ? 'text-green-600' : 'text-red-600'"
+      >
+        {{ message }}
+      </p>
+
       <div class="form-group">
         <div class="form-control">
           <label>Account name</label>
@@ -23,14 +41,20 @@ const formData = reactive({
             placeholder="Enter account name"
             v-model="formData.account_name"
           />
+          <p v-if="errors?.account_name" class="error">
+            {{ errors.account_name[0] }}
+          </p>
         </div>
         <div class="form-control">
           <label>Account website</label>
           <input
-            type="text"
+            type="url"
             placeholder="Enter website"
             v-model="formData.account_website"
           />
+          <p v-if="errors?.account_website" class="error">
+            {{ errors.account_website[0] }}
+          </p>
         </div>
 
         <div class="form-control">
@@ -40,6 +64,9 @@ const formData = reactive({
             placeholder="Enter phone"
             v-model="formData.account_phone"
           />
+          <p v-if="errors?.account_phone" class="error">
+            {{ errors.account_phone[0] }}
+          </p>
         </div>
       </div>
       <div class="form-group">
@@ -50,6 +77,9 @@ const formData = reactive({
             placeholder="Enter deal name"
             v-model="formData.deal_name"
           />
+          <p v-if="errors?.deal_name" class="error">
+            {{ errors.deal_name[0] }}
+          </p>
         </div>
         <div class="form-control">
           <label>Deal stage</label>
@@ -58,10 +88,15 @@ const formData = reactive({
             placeholder="Enter deal stage"
             v-model="formData.deal_stage"
           />
+          <p v-if="errors?.deal_stage" class="error">
+            {{ errors.deal_stage[0] }}
+          </p>
         </div>
       </div>
 
-      <button class="primary-btn" type="primary">Submit</button>
+      <button class="primary-btn" type="primary" :disabled="isLoading">
+        Submit
+      </button>
     </form>
   </main>
 </template>
