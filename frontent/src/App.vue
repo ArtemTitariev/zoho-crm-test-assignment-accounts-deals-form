f1cf7e1e4d9ed7b6f90dae321e04bc28f47d7fb7
Artem Titariev <artem.titariev@gmail.com>
2025-02-22 11:38:14 +0200

    [frontend] Add vue.js project, create form component

---
+++ frontent/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+// import HomeView from "./views/HomeView.vue";
+import FormComponent from "./components/FormComponent.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <FormComponent />
+  </div>
+
+  <!-- <RouterView />  -->
+</template>
---
+++ frontent/src/components/FormComponent.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { reactive } from "vue";
+
+const formData = reactive({
+  account_name: "",
+  account_website: "",
+  account_phone: "",
+  deal_name: "",
+  deal_stage: "",
+});
+</script>
+
+<template>
+  <main>
+    <h1 class="title">Create account and deal</h1>
+
+    <form @submit.prevent="console.log(formData)" class="w-1/2 mx-auto">
+      <div class="form-group">
+        <div class="form-control">
+          <label>Account name</label>
+          <input
+            type="text"
+            placeholder="Enter account name"
+            v-model="formData.account_name"
+          />
+        </div>
+        <div class="form-control">
+          <label>Account website</label>
+          <input
+            type="text"
+            placeholder="Enter website"
+            v-model="formData.account_website"
+          />
+        </div>
+
+        <div class="form-control">
+          <label>Account phone</label>
+          <input
+            type="tel"
+            placeholder="Enter phone"
+            v-model="formData.account_phone"
+          />
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="form-control">
+          <label>Deal name</label>
+          <input
+            type="text"
+            placeholder="Enter deal name"
+            v-model="formData.deal_name"
+          />
+        </div>
+        <div class="form-control">
+          <label>Deal stage</label>
+          <input
+            type="text"
+            placeholder="Enter deal stage"
+            v-model="formData.deal_stage"
+          />
+        </div>
+      </div>
+
+      <button class="primary-btn" type="primary">Submit</button>
+    </form>
+  </main>
+</template>
